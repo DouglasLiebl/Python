f9430fbeff3900b38194a72d698f35545a4e5162
Douglas Liebl <douglasliebl@outlook.com>
2023-01-05 20:24:10 -0300

    Sixth Commit

---
+++ Exercicios/13-Soma de algarismos.py
@@ -0,0 +1,15 @@
+# Programa que soma os algarismos de um numero inserido pelo úsuario
+
+numeroDig = int(input("Insira um numero sem limite de algarismos: "))
+
+soma = 0
+resto = 0
+divisao =1
+
+while divisao != 0:
+    divisao = numeroDig // 10
+    resto = numeroDig % 10
+    soma = soma + resto
+    numeroDig = divisao
+
+print("A soma dos algarismos do número digitado é", soma)    
