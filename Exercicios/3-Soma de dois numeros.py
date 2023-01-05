fc1113fe453981ed336e5b03ee14ce30a2897896
Douglas Liebl <douglasliebl@outlook.com>
2023-01-05 11:09:42 -0300

    Second Commit

---
+++ Exercicios/3-Soma de dois numeros.py
@@ -0,0 +1,11 @@
+# Faça um Programa que peça dois números e imprima a soma.
+
+n1 = input("Digite o primeiro numero: ")
+n2 = input("Digite o segundo numero: ")
+
+n1 = int(n1)
+n2 = int(n2)
+
+soma = n1 +n2
+
+print("A soma dos numeros informados é", soma)
