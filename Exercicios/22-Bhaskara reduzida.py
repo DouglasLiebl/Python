b697f496f8edb674884106237868e80f65bce157
Douglas Liebl <douglasliebl@outlook.com>
2023-01-07 20:52:51 -0300

    Correções após aprender melhor como usar Funções

--- Exercicios/22-Bhaskara reduzida.py
+++ Exercicios/22-Bhaskara reduzida.py
@@ -5,22 +5,22 @@ print("Programa para calculo de equação de Bhaskara")
 a = int(input("Forneça o valor de 'a': "))
 b = int(input("Forneça o valor de 'b': "))
 c = int(input("Forneça o valor de 'c': "))
+deltar = 0
 
-def delta ():
-    global a, b, c
-    return int(math.pow(b, 2) + 4 * a * c)
+def delta (a):
+    deltaR = int(math.pow(b, 2) + 4 * a * c)
+    return deltaR
 
-def raiz ():
-    global a, b, c
-    raiz1 = (-b + math.isqrt(delta())) / (2 * a)
-    raiz2 = (-b - math.isqrt(delta())) / (2 * a)
+def raiz (a):
+    raiz1 = (-b + math.isqrt(delta(a))) / (2 * a)
+    raiz2 = (-b - math.isqrt(delta(a))) / (2 * a)
     return raiz1, raiz2
 
-if delta() < 0:
+if delta(a) < 0:
     print("Esta equação não possui raízes reais")
-if delta() == 0:
-    raiz()
-    print("O valor da raíz é", raiz())
-if delta() > 0:
-    raiz()
-    print("O valor das raízes são", raiz())
+if delta(a) == 0:
+    raiz(a)
+    print("O valor da raíz é", raiz(a))
+if delta(a) > 0:
+    raiz(a)
+    print("O valor das raízes são", raiz(a))
